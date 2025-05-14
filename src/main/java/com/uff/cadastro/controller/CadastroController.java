eab18a35cb444e4e0f50138779f2e47b2f4b62ea
Matheus Alvernaz <msalvernaz13@gmail.com>
2025-05-14 14:09:14 -0300

    edit controllers: add modelandview

--- src/main/java/com/uff/cadastro/controller/CadastroController.java
+++ src/main/java/com/uff/cadastro/controller/CadastroController.java
@@ -4,10 +4,12 @@ import com.uff.cadastro.model.Cadastro;
 import com.uff.cadastro.repository.CadastroRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
 
@@ -22,12 +24,14 @@ public class CadastroController {
     }
 
     @GetMapping
-    public String abrirCadastro() {
-        return "index";
+    public ModelAndView abrirCadastro() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("cadastro", new Cadastro());
+        return modelAndView;
     }
 
     @PostMapping("/confirmacao")
-    public String receberFormulario(
+    public ModelAndView receberFormulario(
             @RequestParam String nome,
             @RequestParam int idade,
             @RequestParam String dataInicio
@@ -39,6 +43,8 @@ public class CadastroController {
 
         cadastroRepository.save(cadastro);
 
-        return "redirect:/cadastro/confirmacao";
+        ModelAndView modelAndView = new ModelAndView("redirect:/cadastro");
+        modelAndView.addObject("cadastro", cadastro);
+        return modelAndView;
     }
 }
--- src/main/java/com/uff/cadastro/controller/ConfirmacaoController.java
+++ src/main/java/com/uff/cadastro/controller/ConfirmacaoController.java
@@ -3,12 +3,14 @@ package com.uff.cadastro.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/cadastro/confirmacao")
 public class ConfirmacaoController {
     @GetMapping
-    public String abrirConfirmacao(){
-        return "confirmacao";
+    public ModelAndView abrirConfirmacao(){
+        ModelAndView modelAndView = new ModelAndView("confirmacao");
+        return modelAndView;
     }
 }

e829f1b86d3e738cf7af15f17a255a76146edd02
Matheus Alvernaz <msalvernaz13@gmail.com>
2025-05-20 14:08:59 -0300

    add CadastroServiceTest, tests: salvar quando valido; excecao quando invalido

---
+++ src/test/java/com/uff/cadastro/service/CadastroServiceTest.java
@@ -0,0 +1,51 @@
+package com.uff.cadastro.service;
+
+import com.uff.cadastro.model.Cadastro;
+import com.uff.cadastro.repository.CadastroRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CadastroServiceTest {
+
+    @Mock
+    CadastroRepository cadastroRepository;
+
+    @InjectMocks
+    CadastroService cadastroService;
+
+    @Test
+    void deveSalvarCadastroQuandoDadosValidos() {
+        Cadastro cadastroValido = new Cadastro();
+        cadastroValido.setNome("Amanda");
+        cadastroValido.setIdade(31);
+        cadastroValido.setDataInicio(LocalDate.now());
+
+        when(cadastroRepository.save(any(Cadastro.class))).thenReturn(cadastroValido);
+
+        Cadastro resultado = cadastroService.save(cadastroValido);
+
+        assertNotNull(resultado, "O cadastro salvo não deveria ser null");
+        assertEquals("Amanda", resultado.getNome());
+
+        verify(cadastroRepository, times(1)).save(cadastroValido);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoSalvarCadastroInvalido() {
+        Cadastro cadastroInvalido = new Cadastro();
+
+        when(cadastroRepository.save(any())).thenThrow(new IllegalArgumentException("Dados Inválidos."));
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            cadastroService.save(cadastroInvalido);
+        }, "Deveria lançar exceção para cadastro inválido");
+}
+}
